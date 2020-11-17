624d6902f2b8bc5931e78d2ac71b24b5dc5db21b
Kara Dietz <kmdietz@edu.uwaterloo.ca>
2020-11-17 15:53:46 -0500

    Update resume

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ function App() {
         <div className="bg-text">
           <h4>Hi :)</h4>
           <h2>My name is Kara Dietz.</h2>
-          <h4>I am a 2B Software Engineering student at the University of Waterloo.</h4>
-          <h6>I have primarily worked with: Java, C/C++, Python, Swift, SQL, AWS, Kafka, & React</h6> 
-          <h6>Currently seeking Fall 2020 internships. Get in touch with me!</h6>
+          <h4>I am a 3A Software Engineering student at the University of Waterloo.</h4>
+          {/*<h6>I have primarily worked with: Java, C/C++, Python, Golang, Swift, SQL, AWS, Kafka</h6> */}
+          <h6>Currently seeking Summer 2021 internships. Get in touch with me!</h6>
           <SocialButton title="Email" url="mailto:kmdietz@uwaterloo.ca"/>
           <div className="divider"/>
           <SocialButton title="LinkedIn" url="https://www.linkedin.com/in/kara-dietz/"/>
@@ -34,8 +34,8 @@ function App() {
           <SocialButton title="Resume" url="./Kara_Dietz_Resume.pdf"/>
           <div className="divider"/>
           <SocialButton title="GitHub" url="https://github.com/karadietz"/>
-          <h6>I'm still exploring where exactly my interests lie in software engineering, but I would love to get more involved in mobile development, gain exposure to more levels of the stack, continue developing my infrastructure skills, and maybe even dabble with design!</h6>
-          <h6>🌱 Environmental sustainability is one of my greatest values, and I hope to contribute my tech skills towards creating sustainable solutions for society.</h6>
+          {/*<h6>I'm still exploring where exactly my interests lie in software engineering, but I would love to get more involved in mobile development, gain exposure to more levels of the stack, continue developing my infrastructure skills, and maybe even dabble with design!</h6>*/}
+          {/*gh6>🌱 Environmental sustainability is one of my greatest values, and I hope to contribute my tech skills towards creating sustainable solutions for society.</h6>*/}
         </div>
       </header>
     </div>
